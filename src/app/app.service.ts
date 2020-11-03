c994ad6b2dc29a041adfe86f430d41f7b20c0bc3
Francesco Oteri <>
2020-11-03 10:55:08 +0100

    feature(app.service): get all covid api created

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+export interface UserLoginBody {
+  username: string;
+  password: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCovid() {
+    const url = 'https://disease.sh/v3/covid-19/all'; // Replace it with your own API path
+    this.http.get(url).toPromise()
+      .then((res: any) => {
+        console.log('RES: ', res);
+        // If you wish to return the body of response only
+        return res.data;
+      });
+  }
 }
